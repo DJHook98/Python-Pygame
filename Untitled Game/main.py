fba8737dd7f04625fc0effbe8201cb428b513a5e
DJHook98 <danhook@live.co.uk>
2014-02-17 21:56:21 +0000

    Added some class test to main.py

--- Untitled Game/main.py
+++ Untitled Game/main.py
@@ -1,2 +1,25 @@
 # *Untiled* pygame file
 
+import pygame, os, random
+from pygame.locals import *
+
+# Initiate PyGame 
+pygame.init()
+
+#Clock setting
+
+
+# Class tests
+class Player(pygame.sprite.Sprite):
+
+	image = pygame.image.load("graphics/player/player-idle.png")
+	image = image.convert_alpha()
+
+	def __init__(self, startpos):
+		pygame.sprite.Sprite.__init__(self, self.groups)
+		self.pos = startpos
+		self.image = Player.image
+		self.rect = self.image.get_rect()
+
+	def update(self):
+		self.rect.center = self.pos
