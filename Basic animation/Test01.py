6642b436e03664de75d112800f5a06e3f5c51000
DJHook98 <danhook@live.co.uk>
2014-02-19 22:50:52 +0000

    Added basic animation files

---
+++ Basic animation/Test01.py
@@ -0,0 +1,37 @@
+## Basic pygame animation
+
+# Import modules
+import pygame, os
+from pygame.locals import *
+
+# Activate pygame
+pygame.init()
+
+# Game window
+DISPLAYSURF = pygame.display.set_mode((640, 470), 0, 32)
+
+# Load images
+ball_load = pygame.image.load("ball_01.png")
+ball = ball_load.convert_alpha()
+ball_x = ball.get_width()
+
+#Variables
+COLOR = (0, 0, 0)
+x = 0
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            os._exit(0)
+
+    DISPLAYSURF.fill(COLOR)
+    DISPLAYSURF.blit(ball, (x, 160))
+
+    x += 1
+
+    if x > 640:
+        x = 0 - ball_x
+
+    pygame.display.update()
