2392df7667a756c25d738b2016c296cd177c5e57
DJHook98 <danhook@live.co.uk>
2014-02-20 13:45:53 +0000

    * Fixed indentation errors in basic template.py

--- PyGame Template(s)/Basic Template.py
+++ PyGame Template(s)/Basic Template.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 pygame.init()
 
 while True:
-	for event in pygame.event.get():
-		if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-			pygame.quit()
-			os._exit(0)
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            pygame.quit()
+            os._exit(0)
