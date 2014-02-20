239b8495a93775fda4f96999050753541e72fad4
DJHook98 <danhook@live.co.uk>
2014-02-20 20:25:35 +0000

    Updated Basic Animation 01

--- Basic animation/Test01.py
+++ Basic animation/Test01.py
@@ -6,32 +6,50 @@ from pygame.locals import *
 
 # Activate pygame
 pygame.init()
+pygame.display.set_caption("Basic Animation Test 01")
 
 # Game window
 DISPLAYSURF = pygame.display.set_mode((640, 480), 0, 32)
 
 # Load images
+# Ball one
 ball_load = pygame.image.load("ball_01.png")
 ball = ball_load.convert_alpha()
 ball_x = ball.get_width()
+ball_y = (DISPLAYSURF.get_height() / 4) * 0.5
+
+# Ball two 
+ball_2_load = pygame.image.load("ball_02.png")
+ball_2 = ball_2_load.convert_alpha()
+ball_2_x = ball_2.get_width()
+ball_2_y = (DISPLAYSURF.get_height() / 4) * 2.5
+
 
 #Variables
 COLOR = (0, 0, 0)
 x = 0
+x_2 = 640 - ball_2_x
+clock = pygame.time.Clock()
 
 while True:
+    clock.tick(30) #The game runs at 30 ticks per second
 
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             pygame.quit()
             os._exit(0)
 
     DISPLAYSURF.fill(COLOR)
-    DISPLAYSURF.blit(ball, (x, 160))
+    DISPLAYSURF.blit(ball, (x, ball_y))
+    DISPLAYSURF.blit(ball_2, (x_2, ball_2_y))
 
     x += 1
+    x_2 -= 1
 
     if x > 640:
         x = 0 - ball_x
 
+    if x_2 < 0 - ball_2_x:
+        x_2 = 640 
+        
     pygame.display.update()
